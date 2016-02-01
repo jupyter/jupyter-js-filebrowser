ba20df579df5fe43f8ed8caa9ec6f6bdda8be456
Steven Silvester <steven.silvester@ieee.org>
2016-02-01 10:41:20 -0600

    Expose widgets to allow file opener to handle widget state

--- src/handler.ts
+++ src/handler.ts
@@ -64,14 +64,6 @@ abstract class AbstractFileHandler implements IMessageFilter {
   }
 
   /**
-   * Get the currently focused widget, or `null`.
-   */
-  get currentWidget(): Widget {
-    return arrays.find(this.widgets,
-      w => w.node.contains(document.activeElement as HTMLElement));
-  }
-
-  /**
    * Get the list of file extensions explicitly supported by the handler.
    */
   get fileExtensions(): string[] {
@@ -83,6 +75,26 @@ abstract class AbstractFileHandler implements IMessageFilter {
    */
   get mimeTypes(): string[] {
     return []
+  }
+
+  /**
+   * Get the current set of widgets managed by the handler.
+   *
+   * #### Notest
+   * This is a read-only property
+   */
+  get widgets(): Widget[] {
+    return this._widgets.splice();
+  }
+
+  /**
+   * Get the contents manager used by the handler.
+   *
+   * #### Notest
+   * This is a read-only property
+   */
+  get manager(): IContentsManager {
+    return this._manager;
   }
 
   /**
@@ -110,7 +122,7 @@ abstract class AbstractFileHandler implements IMessageFilter {
     widget.title.closable = true;
     widget.title.changed.connect(this.titleChanged, this);
     AbstractFileHandler.modelProperty.set(widget, model);
-    this.widgets.push(widget);
+    this._widgets.push(widget);
     installMessageFilter(widget, this);
 
     this.getContents(model).then(contents => {
@@ -157,7 +169,7 @@ abstract class AbstractFileHandler implements IMessageFilter {
       return false;
     }
     widget.dispose();
-    this.widgets.splice(index, 1);
+    this._widgets.splice(index, 1);
     return true;
   }
 
@@ -227,8 +239,8 @@ abstract class AbstractFileHandler implements IMessageFilter {
     }
   }
 
-  protected manager: IContentsManager;
-  protected widgets: Widget[] = [];
+  private _manager: IContentsManager;
+  private _widgets: Widget[] = [];
 }
 
 
